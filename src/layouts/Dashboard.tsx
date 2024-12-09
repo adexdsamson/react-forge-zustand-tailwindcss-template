74cb201f645e34294e218b01884413986fd1aceb
Adediran Adeola <35856417+adexdsamson@users.noreply.github.com>
2024-12-09 14:39:42 +0100

    updates

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -2,39 +2,13 @@ import Container from "@/components/layouts/Container";
 import { Outlet } from "react-router-dom";
 import { SideBar } from "./Sidebar";
 import { Header } from "./Header";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { useEffect, useState } from "react";
 
 export const Dashboard = () => {
   const [isSidebarCollapsed, setSidebarCollapsed] = useState(false);
   const [show, setShow] = useState<boolean>(false);
   const [showSideBarOnSM, setShowSideBarOnSM] = useState<boolean>(false); // Corrected variable name
-
-  // Automatically close sidebar on mobile devices
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 768) {
-        // Adjust this breakpoint as needed
-        setShow(false);
-        setSidebarCollapsed(false);
-        setShowSideBarOnSM(true); // Update showSideBarOnSM state when on small screens
-      } else {
-        setSidebarCollapsed(false);
-        setShow(true);
-        setShowSideBarOnSM(false); // Update showSideBarOnSM state when not on small screens
-      }
-    };
-
-    // Call handleResize on component mount
-    handleResize();
-
-    // Add event listener for window resize
-    window.addEventListener("resize", handleResize);
-
-    // Cleanup event listener on component unmount
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   return (
     <Container
@@ -43,33 +17,13 @@ export const Dashboard = () => {
       fullHeight
       display="flex"
       className="overflow-x-hidden overflow-y-auto dark:bg-slate-950 bg-[#F7F9FE] relative"
+      as={SidebarProvider}
     >
-      {showSideBarOnSM ? (
-        show ? (
-          <SideBar
-            isSidebarCollapsed={isSidebarCollapsed}
-            showSidebarSm={showSideBarOnSM}
-          />
-        ) : (
-          ""
-        )
-      ) : (
-        <SideBar isSidebarCollapsed={isSidebarCollapsed} />
-      )}
-
-      <Container noGutter className="w-full overflow-auto">
-        <Header
-          showSideBar={show}
-          setShow={setShow}
-          showSideBarOnSM={showSideBarOnSM}
-          isSidebarCollapsed={isSidebarCollapsed}
-          setSidebarCollapsed={setSidebarCollapsed}
-        />
-
-        <Container>
+        <SideBar />
+        <main>
+          <SidebarTrigger />
           <Outlet />
-        </Container>
-      </Container>
+        </main>
     </Container>
   );
 };
--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -4,10 +4,24 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import Img from "../assets/react.svg";
 import { MdLogout } from "react-icons/md";
 import { Link, useLocation } from "react-router-dom";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
 
 import { ComponentClass, ComponentProps, FunctionComponent } from "react";
 import { ConfirmAlert } from "@/components/layouts/ConfirmAlert";
 import { IconType } from "react-icons/lib";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { ChevronUp, User2 } from "lucide-react";
 
 type sideBarProps = {
   isSidebarCollapsed: boolean;
@@ -15,17 +29,14 @@ type sideBarProps = {
 };
 
 type NavigationItem = {
-  icon: IconType,
-  title: string,
-  to: string,
-  active: boolean,
-  isImage?: boolean
-}
+  icon: IconType;
+  title: string;
+  to: string;
+  active: boolean;
+  isImage?: boolean;
+};
 
-export const SideBar = ({
-  isSidebarCollapsed,
-  showSidebarSm,
-}: sideBarProps) => {
+export const SideBar = ({}: sideBarProps) => {
   const location = useLocation();
 
   const navigation: NavigationItem[] = [
@@ -44,101 +55,59 @@ export const SideBar = ({
   ];
 
   return (
-    <aside
-      className={`bg-primary ${
-        isSidebarCollapsed
-          ? showSidebarSm
-            ? "w-[100rem]"
-            : "w-20"
-          : "w-[16rem] min-w-[16rem]"
-      }`}
-    >
-      <div className="mt-10 mb-8">
-        <div className="flex items-center justify-between px-3">
-          <div className="h-10 w-20">
-            <img src={Img} className="w-full h-full object-contain" />
-          </div>
-          <div className="h-8 w-8 rounded-full grid place-items-center bg-white/10">
-            <RxHamburgerMenu className="text-white" />
-          </div>
-        </div>
-      </div>
+    <Sidebar collapsible="icon">
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {navigation.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.to}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
 
-      <div className="overflow-auto h-[30rem]">
-        {navigation.map((item, index) => (
-          <SidebarItem
-            key={index}
-            icon={item.icon}
-            title={item.title}
-            to={item.to}
-            active={item.active}
-            isImage={item?.isImage}
-          />
-        ))}
-
-        {/* <SidebarItem
-          icon={IoMail}
-          title="Messaging"
-          to="/dashboard/messaging"
-          active={location.pathname === "/dashboard/messaging"}
-        /> */}
-
-        <ConfirmAlert
-          text="Are you sure you want to log out?"
-          title="Log Out"
-          logout
-          url=""
-          trigger={
-            <div className={`flex items-center gap-3 px-3 py-2 mb-2 mt-5 `}>
-              <SidebarItemIcon icon={MdLogout} />
-              <h6 className="font-medium text-sm text-white">Logout</h6>
-            </div>
-          }
-        />
-      </div>
-    </aside>
-  );
-};
-
-type SidebarItemProps = {
-  title: string;
-  active: boolean;
-  className?: string;
-  isImage?: boolean;
-  to: string;
-  icon: ComponentProps<FunctionComponent<any> | ComponentClass<any, any>>;
-};
-
-const SidebarItem = (props: SidebarItemProps) => {
-  const Icon = props.icon;
-  return (
-    <Link to={props.to}>
-      <div
-        className={`flex items-center gap-3 px-3 py-2 mb-2  ${
-          props.active ? " bg-white/10" : ""
-        } ${props.className ?? ""}`}
-      >
-        <SidebarItemIcon icon={Icon} isImage={props.isImage} />
-        <h6 className="font-medium text-sm text-white">{props.title}</h6>
-      </div>
-    </Link>
-  );
-};
-
-type SidebarItemIconProps = {
-  isImage?: boolean;
-  icon: ComponentProps<FunctionComponent<any> | ComponentClass<any, any>>;
-};
-
-const SidebarItemIcon = (props: SidebarItemIconProps) => {
-  const Icon = props.icon;
-  return (
-    <div className="h-8 w-8 bg-white rounded-md grid place-items-center">
-      {props?.isImage ? (
-        <img src={props.icon} className="h-5 w-5 text-primary" />
-      ) : (
-        <Icon className="h-5 w-5 text-primary" />
-      )}
-    </div>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> Username
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuItem>
+                  <span>Account</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Billing</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
   );
 };
